b6919b025362a9dd2f810931696ccfe279221930
Vinit Patel <vpatel@scitec.com>
2022-02-25 16:30:35 -0500

    Added some colors to plot

--- plot.py
+++ plot.py
@@ -1,16 +1,18 @@
 from Point import Point
 import matplotlib.pyplot as plt
 from random import randint
+from colour import Color
 
-NUM_POINTS = 1_000_000
+NUM_POINTS = 10_000
 
 LOWER_X = -1_000_000_000
 UPPER_X = 1_000_000_000
 LOWER_Y = -1_000_000_000
 UPPER_Y = 1_000_000_000
 
-points = [Point(randint(LOWER_X, UPPER_X), randint(LOWER_Y, UPPER_Y)) for _ in range(NUM_POINTS)]
+points = [Point(randint(LOWER_X, UPPER_X), randint(LOWER_Y, UPPER_Y)) for i in range(NUM_POINTS)]
+colors = [color.hex for color in list(Color("blue").range_to(Color("green"), NUM_POINTS))]
 
-plt.scatter(*zip(*points), s=1)
+plt.scatter(*zip(*points), c=colors, s=1)
 
 plt.show()
